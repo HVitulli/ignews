2040bfbf88553015f8dac0ab0695b4edc795c6d4
HVitulli <hugovitulli@gmail.com>
2021-04-26 19:51:06 -0300

    commit on main

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -41,6 +41,8 @@ export const getServerSideProps: GetServerSideProps = async ({ req, params }) =>
     const session = await getSession({ req });
     const { slug } = params;
 
+    console.log(session)
+
     if (!session?.activeSubscription) {
         return {
             redirect: {
